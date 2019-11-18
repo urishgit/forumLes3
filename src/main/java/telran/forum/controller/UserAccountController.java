137f5c40639c6f01512c451f9045ccf1bdc2f9f1
URI <URI@LAPTOP-FN744CJD>
2019-11-18 08:48:07 +0200

    firstCommit

---
+++ src/main/java/telran/forum/controller/UserAccountController.java
@@ -0,0 +1,65 @@
+package telran.forum.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import telran.forum.dto.UserEditDto;
+import telran.forum.dto.UserProfileDto;
+import telran.forum.dto.UserRegisterDto;
+import telran.forum.service.UserAccountService;
+
+@RestController
+@RequestMapping("/account")
+public class UserAccountController {
+	@Autowired
+	UserAccountService userAccountService;
+	
+	@PostMapping("/user")
+	public UserProfileDto register(@RequestBody UserRegisterDto userRegisterDto) {
+		return userAccountService.register(userRegisterDto);
+	}
+	
+	@PostMapping("/login")
+	public UserProfileDto login(@RequestHeader("Authorization") String token) {
+		return userAccountService.login(token);
+	}
+@PostMapping("/edit")
+UserProfileDto editUser( @RequestHeader("Authorization") String token, @RequestBody  UserEditDto userEditDto)
+{
+	return userAccountService.editUser(token, userEditDto);
+}
+
+	@DeleteMapping("/delete")
+	UserProfileDto removeUser( @RequestHeader("Authorization")  String token)
+	{
+		return userAccountService.removeUser(token);
+	}
+	@PostMapping("/change/password")
+	void changePassword( @RequestHeader("Authorization")  String token,@RequestParam String password)
+	{
+		userAccountService.changePassword(token, password);
+	}
+	@PutMapping("/add/role/{role}")
+	Set<String> addRole(@RequestParam String login,@PathVariable String role,  @RequestHeader("Authorization")  String token)
+	{
+		return userAccountService.addRole(login, role, token);
+	}
+	
+	@PutMapping("/remove/role/{login}")
+	Set<String> removeRole(@RequestParam String login,@PathVariable String role,   @RequestHeader("Authorization") String token)
+	{
+		return userAccountService.removeRole(login, role, token);
+	}
+	
+	
+}
---
+++ src/main/java/telran/forum/service/UserAccountServiceImpl.java
@@ -0,0 +1,177 @@
+package telran.forum.service;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import telran.forum.configuration.AccountConfiguration;
+import telran.forum.dao.UserAccountRepository;
+import telran.forum.dto.UserEditDto;
+import telran.forum.dto.UserProfileDto;
+import telran.forum.dto.UserRegisterDto;
+import telran.forum.exceptions.ForbiddenException;
+import telran.forum.exceptions.UserAuthenticationException;
+import telran.forum.exceptions.UserExistsException;
+import telran.forum.model.UserAccount;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService {
+	
+	@Autowired
+	UserAccountRepository accountRepository;
+	
+	@Autowired
+	AccountConfiguration accountConfiguration;
+
+	@Override
+	public UserProfileDto register(UserRegisterDto userRegisterDto) {
+		if (accountRepository.existsById(userRegisterDto.getLogin())) {
+			throw new UserExistsException();
+		}
+		String hashPassword = BCrypt.hashpw(userRegisterDto.getPassword(), BCrypt.gensalt());
+		UserAccount userAccount = UserAccount.builder()
+									.login(userRegisterDto.getLogin())
+									.password(hashPassword)
+									.firstName(userRegisterDto.getFirstName())
+									.lastName(userRegisterDto.getLastName())
+									.role("User")
+									.expDate(LocalDateTime.now().plusDays(accountConfiguration.getExpPeriod()))
+									.build();
+		accountRepository.save(userAccount);
+		return userAccountToUserProfileDto(userAccount);
+	}
+	
+	private UserProfileDto userAccountToUserProfileDto(UserAccount userAccount) {
+		return UserProfileDto.builder()
+				.login(userAccount.getLogin())
+				.firstName(userAccount.getFirstName())
+				.lastName(userAccount.getLastName())
+				.roles(userAccount.getRoles())
+				.build();
+	}
+
+	@Override
+	public UserProfileDto login(String token) {
+		UserAccountCredentials userAccountCredentials = 
+				accountConfiguration.tokenDecode(token);
+		UserAccount userAccount = 
+				accountRepository.findById(userAccountCredentials.getLogin())
+				.orElseThrow(UserAuthenticationException::new);
+		if (!BCrypt.checkpw(userAccountCredentials.getPassword(), userAccount.getPassword())) {
+			throw new ForbiddenException();
+		}
+		
+		return userAccountToUserProfileDto(userAccount);
+	}
+
+	@Override
+	public UserProfileDto editUser(String token, UserEditDto userEditDto) {
+		UserAccountCredentials userAccountCredentials = 
+				accountConfiguration.tokenDecode(token);
+		UserAccount userAccount = 
+				accountRepository.findById(userAccountCredentials.getLogin())
+				.orElseThrow(UserAuthenticationException::new);
+		if (!BCrypt.checkpw(userAccountCredentials.getPassword(), userAccount.getPassword())) {
+			throw new ForbiddenException();
+		}
+		if(userAccount.getRoles().contains("User") 	)
+		{
+			if(userEditDto.getFirstName()!=null)
+			{
+				userAccount.setFirstName(userEditDto.getFirstName());
+			}
+			if(userEditDto.getLastName()!=null)
+			{
+				userAccount.setLastName(userEditDto.getLastName());
+			}
+		}
+		accountRepository.save(userAccount);
+		return userAccountToUserProfileDto(userAccount);
+	}
+
+	@Override
+	public UserProfileDto removeUser(String token) {
+		UserAccountCredentials userAccountCredentials = 
+				accountConfiguration.tokenDecode(token);
+		UserAccount userAccount = 
+				accountRepository.findById(userAccountCredentials.getLogin())
+				.orElseThrow(UserAuthenticationException::new);
+		if (!BCrypt.checkpw(userAccountCredentials.getPassword(), userAccount.getPassword())) {
+			throw new ForbiddenException();
+		}
+		if(userAccount.getRoles().contains("User") )
+		{
+			accountRepository.delete(userAccount);
+			return userAccountToUserProfileDto(userAccount);
+		}
+		if(userAccount.getRoles().contains("Moderator") || userAccount.getRoles().contains("Administator"))
+		{
+			accountRepository.delete(userAccount);
+			return userAccountToUserProfileDto(userAccount);
+		}
+		return null;
+	}
+
+	@Override
+	public void changePassword(String token, String password) {
+		UserAccountCredentials userAccountCredentials = 
+				accountConfiguration.tokenDecode(token);
+		UserAccount userAccount = 
+				accountRepository.findById(userAccountCredentials.getLogin())
+				.orElseThrow(UserAuthenticationException::new);
+		if (!BCrypt.checkpw(userAccountCredentials.getPassword(), userAccount.getPassword())) {
+			throw new ForbiddenException();
+		}
+		if(userAccount.getRoles().contains("User"))
+		{
+			userAccount.setPassword(password);
+			accountRepository.save(userAccount);
+		}
+		
+	}
+
+	@Override
+	public Set<String> addRole(String login, String role, String token) {
+		UserAccountCredentials userAccountCredentials = 
+				accountConfiguration.tokenDecode(token);
+		UserAccount userAccount = 
+				accountRepository.findById(userAccountCredentials.getLogin())
+				.orElseThrow(UserAuthenticationException::new);
+		if (!BCrypt.checkpw(userAccountCredentials.getPassword(), userAccount.getPassword())) {
+			throw new ForbiddenException();
+		}
+		if(userAccount.getRoles().contains("Moderator") || userAccount.getRoles().contains("Administator"))
+		{
+			UserAccount acc=accountRepository.findById(login).orElseThrow(UserAuthenticationException::new);
+			acc.addRole(role);
+			accountRepository.save(acc);
+			return acc.getRoles();
+		}
+		
+	 return null;
+	}
+
+	@Override
+	public Set<String> removeRole(String login, String role, String token) {
+		UserAccountCredentials userAccountCredentials = 
+				accountConfiguration.tokenDecode(token);
+		UserAccount userAccount = 
+				accountRepository.findById(userAccountCredentials.getLogin())
+				.orElseThrow(UserAuthenticationException::new);
+		if (!BCrypt.checkpw(userAccountCredentials.getPassword(), userAccount.getPassword())) {
+			throw new ForbiddenException();
+		}
+		if(userAccount.getRoles().contains("Moderator") || userAccount.getRoles().contains("Administator"))
+		{
+			UserAccount acc=accountRepository.findById(login).orElseThrow(UserAuthenticationException::new);
+			acc.removeRole(role);
+			accountRepository.save(acc);
+			return acc.getRoles();
+		}
+		return null;
+	}
+
+}
